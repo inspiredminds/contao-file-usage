e26ec26efbf9b4fcaf50d551553cafef4960e600
Ingolf Steinhardt <info@e-spin.de>
2026-08-14 14:21:42 +0200

    Add tests for the filesystem provider
    Covers what the configuration controls: insert tags are found within the configured
    paths (including the line they are used in), files not matching the include patterns or
    matching the exclude patterns are skipped, paths are accepted absolute as well as
    relative to the project dir and paths that do not exist are ignored.

---
+++ tests/Provider/FilesystemProviderTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Contao File Usage extension.
+ *
+ * (c) INSPIRED MINDS
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace InspiredMinds\ContaoFileUsage\Tests\Provider;
+
+use Contao\CoreBundle\Framework\ContaoFramework;
+use InspiredMinds\ContaoFileUsage\Provider\FilesystemProvider;
+use InspiredMinds\ContaoFileUsage\Result\FilesystemResult;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FilesystemProviderTest extends TestCase
+{
+    private const UUID = '58ca4a90-2d30-11e4-8c21-0800200c9a66';
+
+    private string $projectDir;
+
+    protected function setUp(): void
+    {
+        $this->projectDir = sys_get_temp_dir().'/contao-file-usage-'.bin2hex(random_bytes(8));
+
+        (new Filesystem())->mkdir($this->projectDir);
+    }
+
+    protected function tearDown(): void
+    {
+        (new Filesystem())->remove($this->projectDir);
+    }
+
+    public function testFindsInsertTagsWithinTheConfiguredPaths(): void
+    {
+        $this->givenFile('templates/foo.html5', "<div>\n{{image::".self::UUID."?width=1}}\n</div>");
+
+        $results = $this->getProvider(['templates'])->find();
+
+        $this->assertTrue($results->hasResults());
+        $this->assertCount(1, $results[self::UUID]);
+
+        $result = iterator_to_array($results[self::UUID])[0];
+
+        $this->assertInstanceOf(FilesystemResult::class, $result);
+        $this->assertSame('templates/foo.html5', $result->getPath());
+        $this->assertSame(2, $result->getLine());
+    }
+
+    public function testIgnoresFilesNotMatchingTheIncludePatterns(): void
+    {
+        $this->givenFile('templates/foo.txt', '{{file::'.self::UUID.'}}');
+
+        $this->assertFalse($this->getProvider(['templates'])->find()->hasResults());
+    }
+
+    public function testIgnoresFilesMatchingTheExcludePatterns(): void
+    {
+        $this->givenFile('templates/node_modules/foo.html5', '{{file::'.self::UUID.'}}');
+
+        $this->assertFalse($this->getProvider(['templates'])->find()->hasResults());
+    }
+
+    public function testIgnoresPathsOutsideOfTheConfiguredOnes(): void
+    {
+        $this->givenFile('other/foo.html5', '{{file::'.self::UUID.'}}');
+
+        $this->assertFalse($this->getProvider(['templates'])->find()->hasResults());
+    }
+
+    public function testAcceptsAbsolutePaths(): void
+    {
+        $this->givenFile('templates/foo.html5', '{{file::'.self::UUID.'}}');
+
+        $this->assertTrue($this->getProvider([$this->projectDir.'/templates'])->find()->hasResults());
+    }
+
+    public function testSkipsPathsThatDoNotExist(): void
+    {
+        $this->givenFile('templates/foo.html5', '{{file::'.self::UUID.'}}');
+
+        $this->assertTrue($this->getProvider(['templates', 'does/not/exist'])->find()->hasResults());
+    }
+
+    public function testReturnsNoResultsWithoutAnyExistingPath(): void
+    {
+        $this->assertFalse($this->getProvider(['does/not/exist'])->find()->hasResults());
+    }
+
+    /**
+     * @param list<string> $paths
+     */
+    private function getProvider(array $paths): FilesystemProvider
+    {
+        $framework = $this->createMock(ContaoFramework::class);
+        $framework
+            ->expects($this->once())
+            ->method('initialize')
+        ;
+
+        return new FilesystemProvider(
+            $framework,
+            $this->projectDir,
+            'files',
+            $paths,
+            ['~\.(twig|html5|css|scss|js|php)$~i'],
+            ['~/(node_modules|vendor)/~'],
+        );
+    }
+
+    private function givenFile(string $path, string $contents): void
+    {
+        (new Filesystem())->dumpFile($this->projectDir.'/'.$path, $contents);
+    }
+}
